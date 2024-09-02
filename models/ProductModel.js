4884320fb28bdf200b7bb69a90576442492c19d1
MehmetCivril <162918114+MehmetCivril@users.noreply.github.com>
2024-09-02 22:58:45 +0400

    quantity

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -4,6 +4,8 @@ const productSchema = new Schema({
   breedName: { type: String, required: [true, "breedName is required"] },
   price: { type: Number, required: [true, "Price field is required"] },
   age: { type: Number, required: [true, "Age field is required"] },
+  //image: { type: Number, required: [true, "Image field is required"] },
+  quantity: { type: Number, default: 5 },
 })
 
 const Product = model("Product", productSchema)
